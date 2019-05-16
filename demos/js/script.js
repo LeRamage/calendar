f93625732129a07352734dec0cc0ebdb1d44f4d8
Camille Ramage <camille.ramage@VITALLIANCE.LAN>
2019-05-16 17:45:42 +0200

    first commit

---
+++ demos/js/script.js
@@ -0,0 +1,325 @@
+var calendar ;
+var demandeCongesInfo = [];
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendarInteraction.Draggable
+
+    /* initialize the external events
+    -----------------------------------------------------------------*/
+
+    // var containerEl = document.getElementById('external-events-list');
+    // new Draggable(containerEl, {
+    //   itemSelector: '.fc-event',
+    //   eventData: function(eventEl) {
+    //     return {
+    //       title: eventEl.innerText.trim()
+    //     }
+    //   },
+    // });
+
+    //// the individual way to do it
+    var containerEl = document.getElementById('external-events-list');
+    var eventEls = Array.prototype.slice.call(
+      containerEl.querySelectorAll('.fc-event')
+    );
+    eventEls.forEach(function(eventEl) {
+      _id = eventEl.id;
+      new Draggable(eventEl, {
+        eventData: {
+          title: eventEl.innerText.trim(),
+          classNames:_id,
+          id:_id,
+          allDay: false
+        }
+      });
+    });
+
+    /* initialize the calendar
+    -----------------------------------------------------------------*/
+
+    var calendarEl = document.getElementById('calendar');
+    let Cid = "";
+    let element = [];
+
+    calendar = new Calendar(calendarEl, {
+    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'list' ],
+
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+
+    events: [
+      {
+        id:'ferie_WE',
+        title:'Weekend',
+        daysOfWeek: [0,6],
+        classNames:'ferie_WE'
+      }
+    ],
+    
+    firstDay: 1,
+    editable: true,
+    droppable: true, // this allows things to be dropped onto the calendar
+    displayEventTime: false,
+    disableDragging: true,
+
+    eventClick: function(e) {
+      var eventClassNames = e.event.classNames[0];
+      $('#eventClicked').val(e.event);
+
+      if(eventClassNames == 'demandeConge'){
+        demandeCongesInfo.forEach(function(conge){
+          dateConge = new Date(conge["VdateDebut"]);
+          dateConge.setHours(0,0,0,0);
+          if(dateConge.getTime() == e.event.start.getTime())
+            Object.keys(conge).forEach(function(element){
+              $('#'+element).val(conge[element]);
+            })
+            return;
+        }) 
+        $('#modalValidationConge').modal('show')
+      }
+
+      else if(eventClassNames == 'conge'){
+        demandeCongesInfo.forEach(function(conge){
+          dateConge = new Date(conge["VdateDebut"]);
+          dateConge.setHours(0,0,0,0);
+          if(dateConge.getTime() == e.event.start.getTime())
+            Object.keys(conge).forEach(function(element){
+              $('#I'+element.slice(1)).val(conge[element]);
+            })
+            return;
+        })
+        $('#modalInfoConge').modal('show')
+      }
+    },
+
+    eventReceive: function(e){
+      $('#eventReceive').val(e.event);
+    },
+
+    drop: function(arg) {  
+      Cid = arg.draggedEl.id;
+      if(Cid == 'demandeConge'){
+        setTimeout(function(){
+          let start = $('#eventReceive').val().start;
+          let end = $('#eventReceive').val().end;
+          let eventsToRemove = thisDateHasEvent(start,start);
+          if(eventsToRemove.length>0){
+            $('#modalDemandeConge').modal({backdrop: 'static'});
+            $('#modalDemandeConge').modal('show');
+            $('#dateDebut').val(arg["dateStr"]);
+            $('#dateFin').val(arg["dateStr"]);
+          }
+          else{
+            $('#eventReceive').val().remove();
+          }
+        }, 10);        
+      }
+
+      else if(Cid == 'conge'){
+        $('#modalConge').modal({backdrop: 'static'});
+        $('#modalConge').modal('show');
+        $('#CdateDebut').val(arg["dateStr"]);
+        $('#CdateFin').val(arg["dateStr"]);
+      }
+
+      else{
+        start = new Date(arg["dateStr"]);
+        start.setHours(0,0,0,0)
+        let eventsToRemove = thisDateHasEvent(start,start);
+        if(eventsToRemove.length>0){
+          eventsToRemove.forEach(function(eventToRemove){
+            eventToRemove.remove();
+          })
+        }
+        else{
+          setTimeout(function(){
+            $('#eventReceive').val().remove();
+          }, 10);
+        }
+      }
+    },
+    
+    eventRender: function(event) {         
+      element = $(event.el);
+      element.css('border','none'); 
+    },
+
+  });
+  calendar.render();
+  CreateEventPresence();
+});
+
+// --------- Confirmation du formulaire de Congé --------- //
+function confirm_form_Demandeconge(){
+  let event = calendar.getEvents()[calendar.getEvents().length - 1];
+  let start = new Date($('#dateDebut').val());
+  let end = new Date($('#dateFin').val());
+  start.setHours(0,0,0,0);
+  end.setHours(1,0,0,0);
+  event.setDates(start,end)
+ 
+  let info = []
+  $("form#form-demandeConge :input").each(function(){
+    let info_id = 'V'+$(this)[0].id;
+    let val = $(this).val() ;
+    info[info_id] = val;
+  })
+  demandeCongesInfo.push(info);
+
+  let eventsToRemove = thisDateHasEvent(start,end,true);
+  if(eventsToRemove.length>0){
+    eventsToRemove.forEach(function(eventToRemove){
+      eventToRemove.remove();
+    })
+  }
+  $('#modalDemandeConge').modal('hide');
+}
+
+function confirm_form_conge(){
+  let event = calendar.getEvents()[calendar.getEvents().length - 1];
+  let start = new Date($('#CdateDebut').val());
+  let end = new Date($('#CdateFin').val());
+  start.setHours(0,0,0,0);
+  end.setHours(1,0,0,0);
+  event.setDates(start,end)
+ 
+  let info = []
+  $("form#form-Conge :input").each(function(){
+    let info_id = 'V'+$(this)[0].id.slice(1);
+    let val = $(this).val() ;
+    info[info_id] = val;
+  })
+  demandeCongesInfo.push(info);
+
+  let eventsToRemove = thisDateHasEvent(start,end,true);
+  if(eventsToRemove.length>0){
+    eventsToRemove.forEach(function(eventToRemove){
+      eventToRemove.remove();
+    })
+  }
+  $('#modalConge').modal('hide');
+}
+// --------- Annulation d'un Congé --------- //
+function cancelDemandeConge(event){
+  $('#modalDemandeConge').modal('hide');
+  $('#modalConge').modal('hide');
+  event.remove();
+}
+
+// --------- Validation d'une Demande Congé --------- //
+function validation_demande_conge(event){
+    let newEvent = {
+      title:"Congé",
+      start:event.start,
+      end:event.end,
+      classNames:'conge',
+      id: ID(),
+    }
+    calendar.addEvent(newEvent);
+    event.remove();
+    $('#modalValidationConge').modal('hide'); 
+}
+
+// --------- Deny d'un Congé --------- //
+function denyDemandeConge(event){
+  let newEvent = {
+    title:"Congé Refusé",
+    start:event.start,
+    end:event.end,
+    classNames:'congeDeny',
+    id: ID(),
+  }
+  calendar.addEvent(newEvent);
+  event.remove();
+  $('#modalValidationConge').modal('hide'); 
+}
+
+// --------- Supression des évennements après drop d'un external event  --------- //
+function thisDateHasEvent(start,end,isTrue = false){
+  let allEvents = calendar.getEvents();
+  if(isTrue)
+    allEvents.splice(allEvents.length - 1)
+  let daysToCheck = createDateArray(start,end);
+  let eventsToRemove = [];
+
+  if(start.getTime() === end.getTime()  ){ // External Event = 1 journée
+    let isExistsASibling = allEvents.some(function(event){
+      if(event.start.getTime() === start.getTime()){
+        if(event.classNames[0] == 'present')
+          eventsToRemove.push(event);
+      }       
+    })
+  }
+
+  else{ // External Event = plrs journée
+    let isExistsASibling = allEvents.some(function(event){ 
+      if(daysToCheck.find(function(date){
+        return date.getTime() === event.start.getTime();
+      })){
+        if(event.classNames[0] == 'present')
+          eventsToRemove.push(event);
+      }
+    })
+  }
+  return eventsToRemove;
+}
+
+// --------- Obtenir un évenement à une date [date] --------- //
+function getEventByDate(date){
+  let allEvents = calendar.getEvents();
+  let eventAtDate;
+
+  allEvents.forEach(function(event){    
+    if(event.start.getTime() === date.getTime()){
+      eventAtDate = event
+    }
+  }) 
+  return eventAtDate;
+}
+
+// --------- Creer ID unique --------- //
+function ID(){
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+// --------- Tableau contenant toutes les dates de la page courantes / de start à end  --------- //
+function createDateArray(start,end){
+  let
+    dateArray = [],
+    dt = new Date(start);
+
+  while (dt <= end) {
+    dateArray.push(new Date(dt));
+    dt.setDate(dt.getDate() + 1);
+  }
+  return dateArray;
+}
+
+
+// --------- Ajout dynamique de l'évenement présence --------- //
+function CreateEventPresence(){
+  let view = calendar.view;
+  let dates = createDateArray(view.activeStart, view.activeEnd)
+  dates.forEach(function(date){
+    if(![0,6].includes(date.getDay())){
+      let event = [
+        {
+          classNames: 'present',
+          title: "Present(e)",
+          start: date
+        }
+      ]
+      calendar.addEventSource(event)
+    }   
+  })
+}
+
+var ID = function () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
